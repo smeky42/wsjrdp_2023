f1cd5cfb896bd92d06fe69aebbcc600371a680ed
Peter Neubauer <development@smeky.de>
2021-05-16 12:36:28 +0200

    updated licences

--- app/models/concerns/contactable.rb
+++ app/models/concerns/contactable.rb
@@ -1,12 +1,11 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
+#  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
+#  hitobito_wsjrdp_2023 and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+#  https://github.com/hitobito/hitobito_wsjrdp_2023.
 
 require_relative Rails.root.join('app', 'models', 'countries')
-
 module Contactable
 
   extend ActiveSupport::Concern
