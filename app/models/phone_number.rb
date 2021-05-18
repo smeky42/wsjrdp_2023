e10b57d7c308ed4775cb192601d4fb2eaeba0599
Peter Neubauer <development@smeky.de>
2021-05-18 20:01:22 +0200

    german phone number prefix

---
+++ app/models/phone_number.rb
@@ -0,0 +1,53 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+# == Schema Information
+#
+# Table name: phone_numbers
+#
+#  id               :integer          not null, primary key
+#  contactable_type :string(255)      not null
+#  label            :string(255)
+#  number           :string(255)      not null
+#  public           :boolean          default(TRUE), not null
+#  contactable_id   :integer          not null
+#
+# Indexes
+#
+#  index_phone_numbers_on_contactable_id_and_contactable_type  (contactable_id,contactable_type)
+#
+
+class PhoneNumber < ActiveRecord::Base
+
+  include ContactAccount
+
+  self.value_attr = :number
+
+  before_validation :format_number
+
+  validates :number, phone: true
+
+  validates_by_schema
+
+  private
+
+  def format_number
+    # Read Settings again in Wagon to recieve German +49
+    Phonelib.default_country = Settings.phone_number.default_country
+    phone = Phonelib.parse(number)
+    if phone.valid?
+      self.number = phone.international
+    end
+  end
+
+  class << self
+    def predefined_labels
+      Settings.phone_number.predefined_labels
+    end
+  end
+
+end
