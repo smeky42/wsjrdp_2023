bc13b326e90182a5b40057ebc49d9973c9647db4
Peter Neubauer <development@smeky.de>
2021-05-22 22:50:43 +0200

    v1.0.1 fixed index
    added module to personIndex

--- app/indices/person_index.rb
+++ app/indices/person_index.rb
@@ -5,10 +5,12 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_wsjrdp_2023.
 
+module PersonIndex; end
+
 ThinkingSphinx::Index.define_partial :person do
-  indexes :rdp_association,
-          :rdp_association_region,
-          :rdp_association_sub_region,
-          :rdp_association_group,
-          :rdp_association_number
+  indexes rdp_association,
+          rdp_association_region,
+          rdp_association_sub_region,
+          rdp_association_group,
+          rdp_association_number
 end
