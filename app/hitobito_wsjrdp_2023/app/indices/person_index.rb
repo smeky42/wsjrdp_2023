ba41456527c268e7d51f54732422eece4ee97a0d
Peter Neubauer <development@smeky.de>
2021-05-22 22:48:49 +0200

    fixed person index

--- app/hitobito_wsjrdp_2023/app/indices/person_index.rb
+++ app/hitobito_wsjrdp_2023/app/indices/person_index.rb
@@ -1,15 +1,14 @@
+# frozen_string_literal: true
+
 #  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
 #  hitobito_wsjrdp_2023 and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_wsjrdp_2023.
 
-
-module PersonIndex; 
-
 ThinkingSphinx::Index.define_partial :person do
-  indexes :rdp_association, 
-        :rdp_association_region, 
-        :rdp_association_sub_region, 
-        :rdp_association_group, 
-        :rdp_association_number
+  indexes :rdp_association,
+          :rdp_association_region,
+          :rdp_association_sub_region,
+          :rdp_association_group,
+          :rdp_association_number
 end
