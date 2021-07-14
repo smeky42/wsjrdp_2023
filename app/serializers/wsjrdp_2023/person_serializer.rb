c9edaa095c3f1630e2a9275435ad8306e8152459
Peter Neubauer <development@smeky.de>
2021-07-14 22:48:06 +0200

    sub tabs for additional contact and general info

--- app/serializers/wsjrdp_2023/person_serializer.rb
+++ app/serializers/wsjrdp_2023/person_serializer.rb
@@ -1,18 +1,25 @@
+# frozen_string_literal: true
+
 #  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
 #  hitobito_wsjrdp_2023 and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_wsjrdp_2023.
 
 module Wsjrdp2023::PersonSerializer
-    extend ActiveSupport::Concern
-    included do
-      extension(:details) do |_|
-        map_properties :old_id, 
-                :rdp_association, 
-                :rdp_association_region, 
-                :rdp_association_sub_region, 
-                :rdp_association_group, 
-                :rdp_association_number
-      end
+  extend ActiveSupport::Concern
+  included do
+    extension(:details) do |_|
+      map_properties :old_id,
+                     :rdp_association,
+                     :rdp_association_region,
+                     :rdp_association_sub_region,
+                     :rdp_association_group,
+                     :rdp_association_number,
+                     :additional_contact_name_a,
+                     :additional_contact_adress_a,
+                     :additional_contact_name_b,
+                     :additional_contact_adress_b,
+                     :additional_contact_address_info
     end
   end
+end
