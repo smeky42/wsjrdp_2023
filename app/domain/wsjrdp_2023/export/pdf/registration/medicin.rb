fae67370d5756cc55df927ae5e0327c0b139630d
Peter Neubauer <development@smeky.de>
2021-07-18 15:30:34 +0200

    align medicine contract and attributes

--- app/domain/wsjrdp_2023/export/pdf/registration/medicin.rb
+++ app/domain/wsjrdp_2023/export/pdf/registration/medicin.rb
@@ -18,8 +18,7 @@ module Wsjrdp2023
         end
       end
 
-      # rubocop:disable AbcSize
-      # rubocop:disable MethodLength
+      # rubocop:disable AbcSize,MethodLength,MultilineTernaryOperator,CyclomaticComplexity,PerceivedComplexity
       def list
         of_legal_age = false # @person.years.to_i >= 18
         if of_legal_age
@@ -36,8 +35,8 @@ module Wsjrdp2023
                                        [{ content: @person.town + ' den ' \
                                          + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
                                        %w(__________________________ __________________________),
-                                       [{ content: 'Personensorgeberechtigte*r', height: 30 },\
-                                        + 'Personensorgeberechtigte*r'],
+                                       [{ content: @person.additional_contact_name_a, height: 30 },\
+                                        + @person.additional_contact_name_b],
                                        ['______________________________', ''],
                                        [{ content: @person.full_name, height: 30 }, '']
                                      ],
@@ -46,22 +45,17 @@ module Wsjrdp2023
         end
 
 
+
         pdf.start_new_page
-        text 'Gesundheitsfragebogen', size: 14
-        text 'zu Teilnehmer: ' + @person.full_name
-        text 'Personensorgeberechtigte: ' + ' TODO NAME 1 ' + ' TODO NAME 2'
-        text 'im Notfall zu erreichen unter TODO Telefonnummer'
+        text I18n.t('activerecord.attributes.person.medicine_header'), size: 14
+        text 'zu ' + @person.full_name
+        text 'Ansprechpartner im Notfall: ' + @person.additional_contact_name_a + ' und '\
+        + @person.additional_contact_name_b
         pdf.move_down 3.mm
 
-        text 'Damit die Gesundheit der Teilnehmer auch bei medizinischen'\
-        + ' Notfällen gewährleistet werden kann, müssen die folgenden Fragen'\
-        + ' wahrheitsgemäß und vollständig beantwortet werden. Der rdp stellt'\
-        + ' durch technische und organisatorische Maßnahmen sicher, dass der'\
-        + ' Schutz der nachfolgend erhobenen Gesundheitsdaten gewährleistet'\
-        + ' wird. Weitere Informationen finden sich in den Datenschutzhinweisen,'\
-        + ' dort TODO $$$'
+        text I18n.t('activerecord.attributes.person.medicine_info')
 
-        text 'Grundsätzlich:', size: 12
+        text 'Grundsätzlich', size: 12
         pdf.move_down 1.mm
         text 'Änderungen müssen bis zum Antritt der Reise unverzüglich dem Veranstalter/dem '\
         + 'ärztlichem Team über die Anmeldung (anmeldung.worldscoutjamboree.de) mitgeteilt werden.'
@@ -75,174 +69,161 @@ module Wsjrdp2023
         + ' Datenschutzhinweisen im Anhang.'
 
         pdf.move_down 3.mm
-        text 'Schutzimpfungen :', size: 10
-        text 'Vaccinations:', size: 6, style: :italic
-        text 'Nur im Impfausweis dokumentierte Impfungen gelten als durchgeführt.'
+        text I18n.t('activerecord.attributes.person.medicine_vaccination'), size: 10
+        text 'Vaccinations', size: 6, style: :italic
+        text I18n.t('activerecord.attributes.person.medicine_vaccination_help')
         pdf.move_down 1.mm
-        text ' Folgende Impfungen/Grundimmunisierung werden unsererseits empfohlen:'\
-        + ' Hepatitis A & B.'
+        text I18n.t('activerecord.attributes.person.medicine_vaccination_recommendation')
         pdf.move_down 1.mm
-        text 'Außerdem werden reisemedizinisch für Touristen für Südkorea (Stand 31.03.2021) '\
-        + 'weitere Impfungen empfohlen: Typhus, FSME, Jap Enzephaltitis, Meningokokken.'
-        pdf.move_down 1.mm
-        text 'Liste der Impfungen (Impfdatum Monat/Jahr) :'
-        text '
-        ... Liste der Impfungen mit Datum....
+        text 'Impfungen (Impfdatum Monat/Jahr)'
+        text (@person.medicine_vaccination.empty? ?
+          '--' : @person.medicine_vaccination), style: :italic,
+                                                color: '0000ff'
 
-        '
-        text 'Mein Kind hat die von der STIKO empfohlenen Schutzimpfungen:', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_stiko_vaccination'), size: 10
         text 'My child has the vaccinations recommended by the STIKO'\
-        + ' (German Advisory Board for Vaccination):', size: 6, style: :italic
-        text 'ja oder nein TODO Woher weiß ich das, brauchen wir das?'
-        pdf.move_down 1.mm
+        + ' (German Advisory Board for Vaccination)', size: 6, style: :italic
+        text '' + (@person.medicine_stiko_vaccination ? 'JA' : 'NEIN'), style: :italic,
+                                                                        color: '0000ff'
 
-
-        pdf.move_down 5.mm
-        text 'Bekannte Vorerkrankungen / Operationen / Kinderkrankheiten:', size: 10
-        text 'Known pre-existing conditions/surgeries/childhood illnesses:', size: 6, style: :italic
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_preexisting_conditions'), size: 10
+        text 'Known pre-existing conditions/surgeries/childhood illnesses', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        ... Bekannte Vorerkrankungen ....
+        text (@person.medicine_preexisting_conditions.empty? ?
+          '--' : @person.medicine_preexisting_conditions), style: :italic,
+                                                           color: '0000ff'
 
-        '
-        pdf.move_down 1.mm
-        text 'Folgende Auffälligkeiten / Erkrankungen sind bekannt (z.B. Asthma, Reisekrankheit,'\
-        + 'Epilepsie, Angststörung, AD(H)S, erhöhter Betreuungsaufwand, etc.) : ', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_abnormalities'), size: 10
+        text I18n.t('activerecord.attributes.person.medicine_abnormalities_info')
         text 'The following abnormalities / illnesses are known (e.g. asthma, motion sickness, '\
-        + 'epilepsy, anxiety disorder, AD(H)S, increased care needs, etc.):', size: 6,
-                                                                              style: :italic
+        + 'epilepsy, anxiety disorder, AD(H)S, increased care needs, etc.)', size: 6,
+                                                                             style: :italic
         pdf.move_down 1.mm
-        text '
-        ... Auffälligkeiten  ...
+        text (@person.medicine_abnormalities.empty? ?
+          '--' : @person.medicine_abnormalities), style: :italic,
+                                                  color: '0000ff'
 
-        '
-        pdf.move_down 1.mm
-        text 'Es bestehen folgende Allergien (z.B. gegen Medikamente, nachgewiesene '\
-        + 'Lebensmittelallergien, Heuschnupfen, etc.) mit folgenden Symptomen:', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_allergies'), size: 10
+        text I18n.t('activerecord.attributes.person.medicine_allergies_info')
         text 'The following allergies exist (e.g. to medications, proven food allergies, '\
-        + 'hay fever, etc.) with the following symptoms:', size: 6, style: :italic
+        + 'hay fever, etc.) with the following symptoms', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        ..... Allergien ......
+        text (@person.medicine_allergies.empty? ?
+          '--' : @person.medicine_allergies), style: :italic,
+                                              color: '0000ff'
 
-        '
-        pdf.move_down 1.mm
-        text 'Es bestehen folgende Essensbesonderheiten mit folgenden Symptomen:', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_eating_disorders'), size: 10
         text 'The following eating disorders with the following symptoms '\
-        + 'exist:', size: 6, style: :italic
+        + 'exist', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        ..... Allergien ......
+        text (@person.medicine_eating_disorders.empty? ?
+          '--' : @person.medicine_eating_disorders), style: :italic,
+                                                     color: '0000ff'
 
-        '
-
-        pdf.move_down 1.mm
-        text 'Es bestehen folgende Mobilitätsbedürfnisse auf Grund von:', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_mobility_needs'), size: 10
         text 'There are the following mobility needs due to '\
-        + 'exist:', size: 6, style: :italic
+        + 'exist', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        ..... Mobiliätsbedüfnisse ......
+        text (@person.medicine_mobility_needs.empty? ?
+          '--' : @person.medicine_mobility_needs), style: :italic,
+                                                   color: '0000ff'
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_infectious_diseases'), size: 10
+        text 'There is currently an infectious disease, if so which one', size: 6, style: :italic
+        pdf.move_down 1.mm
+        text (@person.medicine_infectious_diseases.empty? ?
+          '--' : @person.medicine_infectious_diseases), style: :italic,
+                                                        color: '0000ff'
 
-        '
-
-        pdf.move_down 1.mm
-        text 'Es besteht zur Zeit eine infektiöse Erkrankung, wenn ja welche:', size: 10
-        text 'There is currently an infectious disease, if so which one:', size: 6, style: :italic
-        pdf.move_down 1.mm
-        text '
-        ........ Erkrankungen ........
-
-        '
-        pdf.move_down 1.mm
-        text 'Mein / Unser Kind befindet sich zur Zeit in ärztlicher Behandlung (behandelnder '\
-        + 'Arzt Facharzt - Name, Kontaktdaten). Es wird bei Bedarf gestattet, dass sich das '\
-        + 'ärztliche Team der Kontingentsleitung mit dem gennannten med. Fachpersonal in '\
-        + 'Kontakt setzten darf, um Gesundheitsdaten / Informationen auzutauschen '\
-        + '(Schweigepflichtsentbindung) :', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_medical_treatment_contact'), size: 10
+        text I18n.t('activerecord.attributes.person.medicine_medical_treatment_contact_info')
+        text I18n.t('activerecord.attributes.person.medicine_medical_treatment_contact_help')
         text 'My / Our child is currently undergoing medical treatment (attending physician / '\
         + 'specialist - name, contact details). If necessary, it is permitted that the medical '\
         + 'team of the contingent management may contact the named medical specialist in order '\
-        + 'to exchange health data / information (release from confidentiality):', size: 6,
-                                                                                   style: :italic
+        + 'to exchange health data / information (release from confidentiality)', size: 6,
+                                                                                  style: :italic
         pdf.move_down 1.mm
-        text '
-        ..... Kontaktdaten Arzt mit Beispieladresse und Tätigkeitsfeld Mail und Telefonummer.......
-
-        '
-        pdf.move_down 1.mm
-        text 'Medikamente:', size: 12
-        text 'Medications:', size: 6, style: :italic
-        pdf.move_down 1.mm
-        text 'Mein / Unser Kind bekommt folgende Medikamente:', size: 12
-        text 'My / Our child is receiving the following medications:', size: 6, style: :italic
-        text 'Dauermedikation (Dosierung / Einnahmeschema / Einnahmegrund):', size: 10
-        text 'Continuous medication (dosage / intake regimen / reason for intake):', size: 6,
-                                                                                     style: :italic
-        pdf.move_down 1.mm
-        text '
-        .....Medikation .....
-
-        '
-        pdf.move_down 1.mm
-        text 'Bedarfsmedikation (Dosierung / Tageshöchstmenge / Einnahmegrund):', size: 10
-        text 'Needs medication (dosage / daily maximum / reason for taking):', size: 6,
-                                                                               style: :italic
-        pdf.move_down 1.mm
-        text '
-        .....Bedarfsmedikation.......
-
-        '
-        pdf.move_down 1.mm
-        text 'Unser Kind nimmt die Medikamente selbst ein: ', size: 10
-        text 'Our child takes the medication himself: ', size: 6, style: :italic
-        pdf.move_down 1.mm
-        text 'ja nein'
-        pdf.move_down 1.mm
-        text 'Mit dem Hausarzt ist abzuklären, ob für die bestehende Dauer- & Bedarfsmedikation'\
-        + ' ein aktueller Medikamentenplan der Medikamente mitgeführt werden muss.'
+        text (@person.medicine_medical_treatment_contact.empty? ?
+          '--' : @person.medicine_medical_treatment_contact), style: :italic,
+                                                              color: '0000ff'
 
         pdf.move_down 3.mm
-        text 'Weiteres:', size: 12
+        text 'Medikamente', size: 12
+        text 'Medications', size: 6, style: :italic
         pdf.move_down 1.mm
-        text 'Bei meinem / unserem Kind ist auf Folgendes zu Achten (z.B. Besonderheiten,'\
-        + ' Einschränkungen, etc.):', size: 10
+        text 'Mein / Unser Kind bekommt folgende Medikamente', size: 12
+        text 'My / Our child is receiving the following medications', size: 6, style: :italic
+        text I18n.t('activerecord.attributes.person.medicine_continous_medication'), size: 10
+        text I18n.t('activerecord.attributes.person.medicine_continous_medication_info')
+        text 'Continuous medication (dosage / intake regimen / reason for intake)', size: 6,
+                                                                                    style: :italic
+        pdf.move_down 1.mm
+        text (@person.medicine_continous_medication.empty? ?
+          '--' : @person.medicine_continous_medication), style: :italic,
+                                                         color: '0000ff'
+
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_needs_medication'), size: 10
+        text I18n.t('activerecord.attributes.person.medicine_needs_medication_info')
+        text 'Needs medication (dosage / daily maximum / reason for taking)', size: 6,
+                                                                              style: :italic
+        pdf.move_down 1.mm
+        text (@person.medicine_needs_medication.empty? ?
+          '--' : @person.medicine_needs_medication), style: :italic,
+                                                     color: '0000ff'
+
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_medications_self_treatment'), size: 10
+        text 'Our child takes the medication himself: ', size: 6, style: :italic
+        pdf.move_down 1.mm
+        text '' + (@person.medicine_medications_self_treatment ? 'JA' : 'NEIN'), style: :italic,
+                                                                                 color: '0000ff'
+
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_medication_help')
+
+
+        pdf.move_down 3.mm
+        text 'Weiteres', size: 12
+        pdf.move_down 1.mm
+        text I18n.t('activerecord.attributes.person.medicine_other'), size: 10
         text 'Please pay attention to the following for my / our child (restrictions'\
         + ' etc.): ', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        .......Achtung Ah.......
+        text (@person.medicine_other.empty? ? '--' : @person.medicine_other), style: :italic,
+                                                                              color: '0000ff'
 
-        '
-        pdf.move_down 1.mm
-        text 'Das ist uns noch wichtig:', size: 10
+        pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_important'), size: 10
         text 'This is also important to us: ', size: 6, style: :italic
         pdf.move_down 1.mm
-        text '
-        ..... Ganz Wichtig .....
-
-        '
-        pdf.move_down 1.mm
-        text 'Im Falle eines Falles:', size: 10
-        pdf.move_down 1.mm
-        text 'Im Falle einer Erkrankung oder eines Unfalles, bei denen durch eine Behandlung oder '\
-        + 'vorläufige Nicht-Behandlung in der Regel keine bleibenden Schäden zu erwarten sind '\
-        + '(Bagatellerkrankungen-, verletzungen, Zecke/Splitter entfernen,...) darf unser Kind '\
-        + 'eigenständig über Behandlungen entscheiden und in medizinische Eingriffe einwilligen, '\
-        + 'da es die für eine solche Entscheidung notwendige persönliche geistige und körperliche '\
-        + 'Reife aufweist. Die Versorgung darf auch von Seiten der Betreuung erfolgen.
-
-        Bei lebensbedrohlichen Erkrankungen / Unfällen entscheidet der behandelnde Arzt vor Ort.'
+        text (@person.medicine_important.empty? ?
+          '--' : @person.medicine_important), style: :italic,
+                                              color: '0000ff'
 
         pdf.move_down 3.mm
-        text 'Wie kann man Dich in Belastungsphasen unterstützen ? (Schokolade, Musik, '\
-        + 'Liebings-Song, etc):', size: 10
+        text 'Im Falle eines Falles', size: 10
         pdf.move_down 1.mm
-        text '
-        ..... Ganz Wichtig .....
-        '
+        text I18n.t('activerecord.attributes.person.medicine_case')
+        pdf.move_down 1.mm
+        text I18n.t('activerecord.attributes.medicine_case_doctor')
 
         pdf.move_down 3.mm
+        text I18n.t('activerecord.attributes.person.medicine_support'), size: 10
+        pdf.move_down 1.mm
+        text (@person.medicine_support.empty? ? '--' : @person.medicine_support), style: :italic,
+                                                                                  color: '0000ff'
+
+
+        pdf.move_down 5.mm
         text '' + (of_legal_age ? 'Ich habe' : 'Wir haben') + ' den Gesundheitsfragebogen '\
         + 'warheitsgemäß ausgefüllt.'
         pdf.move_down 1.mm
@@ -261,10 +242,8 @@ module Wsjrdp2023
         + 'medizin@worldscoutjamboree.de zur Verfügung.'
         text ''
       end
-      # rubocop:enable AbcSize
-      # rubocop:enable MethodLength
     end
-    # rubocop:enable ClassLength
-
+    # rubocop:enable AbcSize,MethodLength,MultilineTernaryOperator,CyclomaticComplexity,PerceivedComplexity
   end
+  # rubocop:enable ClassLength
 end
