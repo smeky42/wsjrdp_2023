ccfc58d488575a52a916141eeea1f50baf119c25
Peter Neubauer <development@smeky.de>
2022-01-19 21:32:15 +0100

    refactored group print

--- app/domain/wsjrdp_2023/export/pdf/group_registration/groupsheet.rb
+++ app/domain/wsjrdp_2023/export/pdf/group_registration/groupsheet.rb
@@ -23,6 +23,14 @@ module Wsjrdp2023
 
           end
 
+        @member_count = @members.count
+        @approved_member_count = @members.where(status: "bestätigt durch Leitung").count
+        if (@member_count != @approved_member_count )
+          bounding_box([0.mm,250.mm], :width => 100.mm, :heigth => 45.mm) do
+            text "Deine Gruppe hat #{@member_count} Mitglieder, du hast aber erst #{@approved_member_count} durch Scannen des QR Codes bestätigt. Du kannst die Gruppenanmeldung erst versenden, wenn alle Gruppenmitglieder bestätigt sind.", size: 14
+          end
+        end
+
         pdf.y = bounds.height - 60
         bounding_box([0, 200.mm], width: bounds.width, height: bounds.height - 250) do
           font_size(8) do
@@ -32,28 +40,38 @@ module Wsjrdp2023
       end
 
       def list
-        member_count = @members.count
-        approved_member_count = @members.where(status: "bestätigt durch Leitung").count
-        if (member_count != approved_member_count )
-          text "Deine Gruppe hat #{member_count} Mitglieder, du hast aber erst #{approved_member_count} durch scannen des QR Codes bestätigt. Du kannst die Gruppenanmeldung erst versenden, wenn alle bestätigt sind.", size: 14
-        end
+  
 
         pdf.move_down 3.mm
-        text "Gruppenanmeldung - #{@group.name}", size: 12
+        text "Gruppenanmeldung - #{@group.name} - #{@members.count} Mitglieder", size: 12
         
         pdf.move_down 3.mm
-        text "Um die Anmeldung für alle Teilnehmenden der Unit #{@group.name} abzuschließen muss diese Gruppenanmeldung unterschrieben an die obenstehende Adresse verschickt werden."
+        text "Um die Anmeldung für alle Teilnehmenden der Unit #{@group.name} abzuschließen, muss diese Gruppenanmeldung unterschrieben und mit den orginalen Einzelanmeldungen an die obenstehende Adresse geschickt werden."
 
         pdf.move_down 3.mm
         text 'Hiermit bestätige ich, dass ich die Anmeldeunterlagen folgender Gruppenmitglieder im Orginal geprüft habe:'
 
-        @members.each do |person|
-          text "#{person.id} - #{person.first_name} #{person.last_name} , geb. am #{person.birthday.strftime('%d.%m.%y')}"
+        pdf.move_down 3.mm
+        table_data = Array.new
+        for i in 0..@members.count
+          l_member = @members[i]
+          r_member = @members[i+1]
+          if i.even? 
+            if !r_member.nil?
+              table_data << ["#{l_member.id}:", "#{l_member.first_name} #{l_member.last_name}" ,", geb. am #{l_member.birthday.strftime('%d.%m.%y')}"," ", "#{r_member.id}:", "#{r_member.first_name} #{r_member.last_name}" ,", geb. am #{r_member.birthday.strftime('%d.%m.%y')}"]
+            else 
+              table_data << ["#{l_member.id}:", "#{l_member.first_name} #{l_member.last_name}" ,", geb. am #{l_member.birthday.strftime('%d.%m.%y')}"," ", " ", " " ," "]
+            end
+          end 
         end 
+        
+        pdf.table(table_data, :width => 500, :cell_style => {padding: 1, border_width: 0,
+                                                    inline_format: true  })
+       
 
         pdf.move_down 3.mm
         
-        text 'Insbesondere habe ich geprüft, dass'
+        text 'Ich bestätige hiermit die oben genannten Gruppenmitglieder und habe insbesondere geprüft, dass'
         text '- die "Anmeldung" vollständig und korrekt unterschrieben ist'
         text '- das "Sepa-Mandat" vollständig und korrekt unterschrieben ist'
         text '- keine Änderungen an den Orginaldokumenten vorgenommen wurden'
@@ -61,21 +79,26 @@ module Wsjrdp2023
         text '- Geburtsdatum auf Ausweis und Anmeldung übereinstimmen'
 
         pdf.move_down 3.mm
-        text 'Die Anmeldungen und die Sepa-Mandate aller, oben aufgelisteten, Gruppenmitglieder liegen der Gruppenanmeldung im Orginal bei.'
-
-        pdf.move_down 3.mm
+        text 'Die Anmeldungen und die Sepa-Mandate aller, oben aufgelisteten Gruppenmitglieder liegen der Gruppenanmeldung im Orginal bei.'
         text 'Den Medizinbogen aller Gruppenmitglieder habe ich erhalten und die Unterschriften geprüft.'
-        text '(Der Medizinbogen bleibt bei den jeweilig verantwortlichen Gruppenleitern und soll nicht mit versand werden.)'
+        text '(Der Medizinbogen bleibt bei den jeweiligen verantwortlichen Gruppenleiter*innen und wird nicht mit versandt.)'
 
-        pdf.make_table([
-                                       [{ content: '', height: 30 }],
-                                       %w(____________________________________________________),
-                                       [{ content: 'Ort, Datum, Name des Leiters', height: 30 }],
-                                       ['______________________________', ''],
-                                       [{ content: 'Unterschrift', height: 30 }, '']
-                                     ],
-                                     cell_style: { width: 240, padding: 1, border_width: 0,
-                                                   inline_format: true }).draw 
+        pdf.move_down 10.mm
+        table_data = Array.new
+        table_data << ['______________________________', '______________________________' , '______________________________']
+        table_data << ['Ort, Datum', 'Name Unitleitung' , 'Unterschrift']
+        pdf.table(table_data, :width => 500, :cell_style => {padding: 1, border_width: 0,
+                                                    inline_format: true  })
+        # pdf.make_table([
+        #                                [{ content: '', height: 30 }], %w(______________________________),
+        #                                [{ content: 'Ort, Datum', height: 30 }],
+        #                                ['______________________________', ''],
+        #                                [{ content: 'Name Unitleitung', height: 30 }, ''],
+        #                                ['______________________________', ''],
+        #                                [{ content: 'Unterschrift', height: 30 }, '']
+        #                              ],
+        #                              cell_style: { width: 240, padding: 1, border_width: 0,
+        #                                            inline_format: true }).draw 
 
 
       text ''
