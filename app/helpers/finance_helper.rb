e66196f32df7f701a126e603405a5bd3c6068d2e
Peter Neubauer <development@smeky.de>
2023-02-08 20:38:19 +0100

    changed accounting plan

--- app/helpers/finance_helper.rb
+++ app/helpers/finance_helper.rb
@@ -15,28 +15,29 @@ module FinanceHelper
       [
         [' ', 'Gesamtbeitrag', 'Dez 21', 'Jan 22', 'Feb 22', 'Mär 22', 'Apr 22', 'Mai 22', 'Jun 22',
          'Jul 22', 'Aug 22', 'Sep 22', 'Okt 22', 'Nov 22', 'Dez 22', 'Jan 23', 'Feb 23', 'Mär 23',
-         'Apr 23', 'Mai 23'],
+         'Apr 23', 'Mai 23', 'Juni 23'],
         ['Teilnehmende*r', ' 4.100 € ', ' 150 € ', ' 150 € ', ' 150 € ', ' 150 € ', ' 150 € ',
          ' 150 € ', ' 150 € ', ' 250 € ', ' 250 € ', ' 250 € ', ' 250 € ', ' 250 € ', ' 300 € ',
-         ' 300 € ', ' 300 € ', ' 300 € ', ' 300 € ', ' 300 € '],
+         ' 300 € ', ' 200 € ', ' 300 € ', ' 300 € ', ' 300 € ', ' 100 € '],
         ['Unit Leitung', ' 3.250 € ', ' 150 € ', ' 150 € ', ' 150 € ', ' 150 € ', ' 150 € ',
          ' 150 € ', ' 150 € ', ' 200 € ', ' 200 € ', ' 200 € ', ' 200 € ', ' 200 € ', ' 200 € ',
-         ' 250 € ', ' 250 € ', ' 250 € ', ' 250 € ', ' -   € '],
+         ' 250 € ', ' 250 € ', ' 250 € ', ' 250 € ', ' -   € ', ' -   € '],
         ['IST', ' 1.650 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ',
          ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ',
-         ' 100 € ', ' 100 € ', ' 50 € ', ' -   € '],
+         ' 100 € ', ' 100 € ', ' 50 € ', ' -   € ', ' -   € '],
         ['Kontingentsteam', ' 1.300 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ',
          ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ', ' 100 € ',
-         ' -   € ', ' -   € ', ' -   € ', ' -   € ', ' -   € ']
+         ' -   € ', ' -   € ', ' -   € ', ' -   € ', ' -   € ', ' -   € ']
       ]
     end
     # rubocop:enable Metrics/MethodLength
 
     def ammount(ammount)
-      if ammount == "- € "
+      if ammount == '- € '
         return 0
       end
-      ammount.gsub(".","").split(" ")[0].to_i * 100
+
+      ammount.gsub('.', '').split(' ')[0].to_i * 100
     end
 
     def payment_by_month(role, month)
@@ -48,31 +49,34 @@ module FinanceHelper
     end
 
     def payment_data_till_date(role, end_date)
-      start_date = Time.new(2021,12,4)
+      start_date = Time.new(2021, 12, 4)
       month = (end_date.year * 12 + end_date.month) - (start_date.year * 12 + start_date.month)
       payment_data_by(role)[0..month]
     end
 
-
     def accounting_balance
       balance = - total_payment_by(@person.role_wish)
       accountEntries = accounting_entries
-      accountEntries.each {|x|  
+      accountEntries.each do |x|
         balance -= x.ammount
-      }
+      end
       balance
     end
 
-    def dept(month) 
-      (total_payment_by(@person.role_wish) + payment_data_till_month(@person.role_wish, month).inject(0){|sum,x| sum + x })
+    def dept(month)
+      (total_payment_by(@person.role_wish) + payment_data_till_month(@person.role_wish,
+                                                                     month).inject(0) do |sum, x|
+                                               sum + x
+                                             end)
     end
 
     def total_payment_by(role)
       -1 * ammount(payment_array_by(role)[1][1])
     end
-    def  payment_data_by(role)
+
+    def payment_data_by(role)
       payment_array_by_role = payment_array_by(role)
-      payment_array_by_role[1].drop(2).map{|x| ammount(x)}
+      payment_array_by_role[1].drop(2).map { |x| ammount(x) }
     end
 
     def payment_array_by(role)
