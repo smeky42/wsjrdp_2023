40f38a9272a38dc2e8c61d938685a58e8126ccae
Peter Neubauer <development@smeky.de>
2023-02-11 15:21:10 +0100

    extended accounting table till june

--- app/controllers/person/accounting_controller.rb
+++ app/controllers/person/accounting_controller.rb
@@ -16,8 +16,8 @@ class Person::AccountingController < ApplicationController
     @person ||= group.people.find(params[:id])
 
     unless authorize_view
-      return 
-    end 
+      return
+    end
 
     @authorize_view = authorize_view
     @accounting = accounting
@@ -26,7 +26,7 @@ class Person::AccountingController < ApplicationController
     @accounting_payment_array = accounting_payment_array
     @accounting_balance = get_number_to_currency(- accounting_balance)
     @next_payment = get_number_to_currency(next_payment)
-    @possible_sepa_states = Settings.person.sepa_status 
+    @possible_sepa_states = Settings.person.sepa_status
     save_put
   end
 
@@ -35,25 +35,26 @@ class Person::AccountingController < ApplicationController
     current_user.role?('Group::Root::Leader') ||
     current_user.role?('Group::UnitSupport::Leader') ||
     current_user.role?('Group::UnitSupport::Member') ||
-    current_user.role?('Group::Ist::Leader') || 
-    current_user == @person )
-  end 
+    current_user.role?('Group::Ist::Leader') ||
+    current_user == @person)
+  end
 
   def accounting_value(value)
-    number_to_currency(value.to_f / 100, :separator => ",", :delimiter => ".", format: "%n %u")
+    number_to_currency(value.to_f / 100, separator: ',', delimiter: '.', format: '%n %u')
   end
   helper_method :accounting_value
 
   private
-  #ToDo dynamic
+
+  # TODO: dynamic
   def next_payment
     to_pay = dept(0) + accounting_balance
 
     if to_pay < 0
       to_pay = 0
-    end 
-    to_pay 
-  end 
+    end
+    to_pay
+  end
 
   def entry
     @person ||= Person.find(params[:id])
@@ -68,54 +69,51 @@ class Person::AccountingController < ApplicationController
     authorize!(:edit, entry)
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity,Metrics/MethodLength,Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
   def save_put
     if @accounting && request.put? && !params[:accounting_comment].nil? && !params[:accounting_ammount].nil?
       AccountingEntries.create(id: AccountingEntries.last.id + 1,
-                           subject_id: @person.id,
-                           author_id: current_user.id,
-                           ammount: params[:accounting_ammount],
-                           comment: params[:accounting_comment],
-                           created_at: DateTime.now)
-      
+                               subject_id: @person.id,
+                               author_id: current_user.id,
+                               ammount: params[:accounting_ammount],
+                               comment: params[:accounting_comment],
+                               created_at: DateTime.now)
+
       @person.sepa_status = params[:sepa_status]
       @person.save
-      
+
       flash[:notice] =
-      "Buchung #{params[:accounting_comment]} in Höhe von #{get_number_to_currency(params[:accounting_ammount].to_i)} erfolgreich angelegt! Status auf #{params[:sepa_status]} gesetzt."
-      redirect_back(fallback_location: "/")
+        "Buchung #{params[:accounting_comment]} in Höhe von #{get_number_to_currency(params[:accounting_ammount].to_i)} erfolgreich angelegt! Status auf #{params[:sepa_status]} gesetzt."
+      redirect_back(fallback_location: '/')
     end
   end
-  # rubocop:enable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity,Metrics/MethodLength,Metrics/AbcSize
+  # rubocop:enable ,Metrics/MethodLength,Metrics/AbcSize
 
   def accounting_entries
     AccountingEntries.where(subject_id: @person.id)
   end
 
   def accounting_payment_array
-    array = Array.new
-    start_date = Date.new(2021,12,5)
-    payment_data = payment_data_till_date(@person.role_wish, Date.new(2023,5,6))
-    payment_data.each_with_index { |data,x| 
-        array.push({
-            month: start_date + x.months,
-            ammount: get_number_to_currency(payment_by_month(@person.role_wish, x)),
-            total: get_number_to_currency(dept(x)),
-        }
-      ) 
-    }
+    array = []
+    start_date = Date.new(2021, 12, 5)
+    payment_data = payment_data_till_date(@person.role_wish, Date.new(2023, 6, 6))
+    payment_data.each_with_index do |_data, x|
+      array.push({
+                   month: start_date + x.months,
+                   ammount: get_number_to_currency(payment_by_month(@person.role_wish, x)),
+                   total: get_number_to_currency(dept(x))
+                 })
+    end
 
     array
   end
 
-
-
   def get_text_to_currency(number)
-    number_to_currency(number.to_f / 100, :separator => ",", :delimiter => ".", format: "%n %u")
+    number_to_currency(number.to_f / 100, separator: ',', delimiter: '.', format: '%n %u')
   end
 
   def get_number_to_currency(number)
-    number_to_currency(number.to_f / 100, :separator => ",", :delimiter => ".", format: "%n %u")
+    number_to_currency(number.to_f / 100, separator: ',', delimiter: '.', format: '%n %u')
   end
 
   def get_payment_value
